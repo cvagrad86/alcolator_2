d52ccd0611712cc21b295d5af583d1e6862dbc0b
Eric Chamberlin <echamberlin@me.com>
2015-11-11 09:15:58 +0100

    moving on

---
+++ alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  alcolator
+//
+//  Created by Eric Chamberlin on 11/11/15.
+//  Copyright © 2015 Eric Chamberlin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
